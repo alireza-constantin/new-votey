c8b3cb8d03e9b04856b9d5fb898d33703078dddb
alireza <alirezasoheili1224@yahoo.com>
2023-08-14 11:56:23 +0330

    trpc up and running

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
+import { trpc } from '@/utils/trpc'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const hello = trpc.greeting.useQuery({name: 'Alireza'})
+
   return (
     <div>
-      hello How are you
+       {hello.data?.text} 
     </div>
   )
 }
